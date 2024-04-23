7f1601c1999a540b3529c9a72f35ab45c3e100f5
Vladiwostok <rickvsprot@protonmail.com>
2024-04-23 15:17:05 +0300

    Add healthcheck controller

---
+++ src/main/kotlin/com/communityhub/controller/HealthController.kt
@@ -0,0 +1,14 @@
+package com.communityhub.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class HealthController {
+    @GetMapping
+    fun test(): String {
+        return "OK"
+    }
+}
