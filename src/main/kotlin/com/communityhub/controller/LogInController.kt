39cb79d3ab597574813388d866426ab0948c30e3
Vladiwostok <rickvsprot@protonmail.com>
2024-05-04 12:29:14 +0300

    Add cors header for login route

--- src/main/kotlin/com/communityhub/controller/LogInController.kt
+++ src/main/kotlin/com/communityhub/controller/LogInController.kt
@@ -4,6 +4,7 @@ import com.communityhub.service.CHubUserService
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.security.SecurityScheme
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -23,8 +24,10 @@ class LogInController(private val cHubUserService: CHubUserService) {
     fun login(@RequestParam name: String, @RequestParam password: String): ResponseEntity<String> {
         return try {
             val token = cHubUserService.updateUserJwtToken(name)
-//            ResponseEntity.ok(token)
-            ResponseEntity.ok().header("Authorization", token).build()
+            ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION, token)
+                .header(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+                .build()
         } catch (e: Exception) {
             ResponseEntity.status(HttpStatus.NOT_FOUND).build()
         }
