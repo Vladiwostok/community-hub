75a80a4e9ac91fa92085d2b75aec29b8d5faf181
Vladiwostok <rickvsprot@protonmail.com>
2024-05-04 20:50:29 +0300

    Fix cors policy

--- src/main/kotlin/com/communityhub/controller/CorsFilter.kt
+++ src/main/kotlin/com/communityhub/controller/CorsFilter.kt
@@ -12,9 +12,12 @@ import org.springframework.stereotype.Component
 class CorsFilter : Filter {
     override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
         val res = response as HttpServletResponse
-        res.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+        res.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:3000")
         res.setHeader(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization, Content-Type")
         res.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE")
+        res.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Authorization, Content-Type")
+        res.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true")
+        res.setHeader(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600")
         chain?.doFilter(request, response)
     }
 }
