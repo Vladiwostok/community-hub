8026265fd4a5110204142011862914457b0af46e
Vladiwostok <rickvsprot@protonmail.com>
2024-05-04 13:00:36 +0300

    Fix cors header filter

--- src/main/kotlin/com/communityhub/controller/CorsFilter.kt
+++ src/main/kotlin/com/communityhub/controller/CorsFilter.kt
@@ -4,11 +4,11 @@ import jakarta.servlet.Filter
 import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
-import jakarta.servlet.annotation.WebFilter
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Component
 
-@WebFilter("/*")
+@Component
 class CorsFilter : Filter {
     override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
         val res = response as HttpServletResponse
